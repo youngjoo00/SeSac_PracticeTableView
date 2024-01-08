45075866c70d12258df1ab287915327fcff67dab
youngjoo00 <yungjoo0624@gmail.com>
2024-01-08 23:57:56 +0900

    design: 여행 매거진 화면 추가

---
+++ SeSac_PracticeTableView/MagazineInfoTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MagazineInfoTableViewCell.swift
+//  SeSac_PracticeTableView
+//
+//  Created by youngjoo on 1/8/24.
+//
+
+import UIKit
+
+class MagazineInfoTableViewCell: UITableViewCell {
+    
+    @IBOutlet var magazineImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subtitleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+}
